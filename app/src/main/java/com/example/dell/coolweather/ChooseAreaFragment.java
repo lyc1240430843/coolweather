92636a1cd5e833f3b3b909f975058e6c8e6e685c
Tony <tony@gmail.com>
2018-04-20 23:25:35 +0800

    完成检索省市县放到碎片

---
+++ app/src/main/java/com/example/dell/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,232 @@
+package com.example.dell.coolweather;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dell.coolweather.db.City;
+import com.example.dell.coolweather.db.Country;
+import com.example.dell.coolweather.db.Province;
+import com.example.dell.coolweather.util.HttpUtil;
+import com.example.dell.coolweather.util.Utility;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by dell on 2018/4/20.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTRY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    //这是传入当前listView的参数列表
+    private List<String> dataList = new ArrayList<>();
+
+    private List<Province> provinceList;
+    private List<City> cityList;
+    private List<Country> countryList;
+
+    private Province selectedProvince;
+    private City selectedCity;
+    //当前选中的级别
+    private int currentLevel;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        titleText = (TextView)view.findViewById(R.id.title_text);
+        backButton = (Button)view.findViewById(R.id.back_button);
+        listView = (ListView)view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //设置listView的选项界面点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (currentLevel==LEVEL_PROVINCE){
+                    selectedProvince = provinceList.get(position);
+                    queryCities();
+                }else if(currentLevel==LEVEL_CITY){
+                    selectedCity = cityList.get(position);
+                    queryCountry();
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentLevel==LEVEL_COUNTRY){
+                    queryCities();
+                }else if(currentLevel==LEVEL_CITY){
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+    /**
+     * 查询省，优先从数据库查，如果没有再去服务器
+     */
+    private void queryProvinces(){
+        titleText.setText("中国");
+        //第一个菜单没有返回键所以设置隐藏
+        backButton.setVisibility(View.GONE);
+        //TODO 待测试 这里DataSupport为何不能换成Province
+        //调用LitePal的查询接口从数据库中取值给省列表，用于下面循环给dataList赋值  先记住是固定写法
+        provinceList = DataSupport.findAll(Province.class);
+        //如果数据库没有就从网上取
+        if (provinceList.size()>0){
+            dataList.clear();
+            //循环给dataList加数据   先记住是固定写法
+            for (Province province : provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            //给适配器通知已改变数据的值
+            adapter.notifyDataSetChanged();
+            //TODO 这句啥意思
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        }else{
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+    }
+
+    /**
+     * 查询市，优先从数据库查，如果没有再去服务器
+     */
+
+    private void queryCities(){
+        titleText.setText(selectedProvince.getProvinceName());
+        //TODO 这里where为何是 provinceid 此参数并不存在 调试可改为id或者后面改成getCode尝试
+        cityList = DataSupport.where("provinceid = ?",String.valueOf(selectedProvince.getId())).find(City.class);
+        if (cityList.size()>0){
+            dataList.clear();
+            for (City city : cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        }else {
+            String address = "http://guoli/tech/api/china/"+selectedProvince.getProvinceCode();
+            queryFromServer(address,"city");
+        }
+    }
+
+    /**
+     * 查询县，优先从数据库查，如果没有再去服务器
+     */
+
+    private void queryCountry(){
+        titleText.setText(selectedCity.getCityName());
+        countryList = DataSupport.where("cityid = ?",String.valueOf(selectedCity.getId())).find(Country.class);
+        if (countryList.size()>0){
+            dataList.clear();
+            for (Country country : countryList){
+                dataList.add(country.getCountryName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_COUNTRY;
+        }else{
+            String address = "http://guolin/tech/api/china"+selectedProvince.getProvinceCode()+selectedCity.getCityCode();
+            queryFromServer(address,"country");
+        }
+    }
+    /**
+     * 根据传入地址和类型从服务器查询省市县
+     */
+    private void queryFromServer(String address,final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                //TODO 这里写的和书上不一样，可能会错在这
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        if ("province".equals(type)){
+                            Utility.handleProvinceResponse(responseText);
+                            queryProvinces();
+                        } else if ("city".equals(type)){
+                            Utility.handleCityResponse(responseText,selectedProvince.getId());
+                            queryCities();
+                        } else if ("country".equals(type)){
+                            Utility.handleCountryResponse(responseText,selectedCity.getId());
+                            queryCountry();
+                        }
+                    }
+                });
+
+            }
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 开启进度框
+     */
+    private void showProgressDialog(){
+        if (progressDialog==null){
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载");
+            progressDialog.setCancelable(false);
+        }
+        progressDialog.show();
+    }
+
+    /**
+     * 关闭进度框
+     */
+    private void closeProgressDialog(){
+        if (progressDialog != null){
+            progressDialog.dismiss();
+        }
+    }
+}
